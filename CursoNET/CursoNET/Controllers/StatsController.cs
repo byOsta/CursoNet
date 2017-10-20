e76c1fe176444c357b88b6490974869e6470e7d8
Jorge Osta <jorgeosta04@gmail.com>
2017-10-20 20:19:33 +0200

    Added StatsController and Stats Index

---
+++ CursoNET/CursoNET/Controllers/StatsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoNET.Controllers
+{
+    public class StatsController : Controller
+    {
+        // GET: Stats
+        public ActionResult Index()
+        {
+            ViewBag.LastDayUploads = 1;
+            ViewBag.LastWeekUploads = 7;
+            ViewBag.LastMonthUploads = 23;
+
+            return View();
+        }
+    }
+}
